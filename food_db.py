b4908a46aa1da9aaafb5b2a4ce30425d883861e3
Pooya <p.j@mailbox.org>
2020-05-24 00:36:22 +0200

    added databse connect for the food_db, user_db still has problem in bmr function

--- food_db.py
+++ food_db.py
@@ -1,6 +1,22 @@
 import sqlite3
 
 
+def connect():
+    conn = sqlite3.connect(food_db, timeout=None, isolation_level=None,
+                           detect_types=None, factory=None)
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE if NOT EXISTS food_tbl (
+                                    id INTEGER,
+                                    Food_name TEXT,
+                                    Fat INTEGER,
+                                    Protein INTEGER,
+                                    Carbs INTEGER,
+                                    Calories INTEGER,
+                                    )""")
+    conn.commit()
+    conn.close()
+
+
 class food:
     def __init__(self, food_name, food_fat, food_protein, food_carbs):
         self.food_name = food_name
@@ -9,9 +25,16 @@ class food:
         self.food_carbs = food_carbs
 
     def food_calories(self):
-        self.food_calories = 9*self.food_fat + 4*(self.food_protein+self.food_carbs)
-        return self.food_calories
+        food_calories = 9*self.food_fat + 4*(self.food_protein+self.food_carbs)
+        return food_calories
+
+
+def food_input():
+    food_name = input("Please enter the food's name:")
+    food_fat = input("Please enter it's fat value:")
+    food_protein = input("Please enter it's protein value:")
+    food_carbs = input("Please enter it's carbihydrate value:")
 
 
 food_1 = food("chicken breast", 2, 21, 0)
-print(food_1.food_name, food_1.food_calories() )
+print(food_1.food_name, food_1.food_calories())
--- user_db.py
+++ user_db.py
@@ -2,12 +2,29 @@ import sqlite3
 
 
 class user:
-    def __init__(self, usr_name="", email="", usr_age, usr_height, usr_activity_level):
+    def __init__(self,  usr_name, usr_gender, usr_age, usr_height, usr_weight, usr_activity_level=0, usr_fat=0):
         self.usr_name = usr_name
-        self.age = usr_age
-        self.height = usr_height
+        self.usr_gender = usr_gender
+        self.age = usr_age  # years
+        self.usr_weight = usr_weight  # kg
+        self.height = usr_height  # centimeters
         self.activity_level = usr_activity_level
+        self.usr_fat = usr_fat  # body fat percentage
 
-        def bmr(self):
-            bmr = self.age*1 + self.height*2 + self.activity_level*3
-            return bmr
+        def bmr_kmca(self):
+            # Katch-McArdle formula
+            bmr_kmca = 370 + 216*(self.usr_weight*(1-self.usr_fat/100))
+            return bmr_kmca
+
+        def bmr_mif(self):
+            # The Mifflin St Jeor Equation formula
+            if self.usr_gender == "male":
+                s = 5
+            else:
+                s = -161
+            bmr _mif = 10 + 6.25*self.usr_height-5*self.usr_age + s
+            return bmr_mif
+
+
+user_1 = user("Pooya", "male", 36, 181, 83)
+print(user_1.usr_name,  user_1.usr_gender, user_1.usr_weight, user_1.bmr_kmca())
