dd2d838e502a14b73bd62ce5eae546b4828dbdf7
Pooya <p.j@mailbox.org>
2020-05-23 21:05:54 +0200

    food_db.py updated

--- food_db.py
+++ food_db.py
@@ -2,11 +2,16 @@ import sqlite3
 
 
 class food:
-    def __init__(self, fat, protein, carbs):
-        self.fat = fat
-        self.protein = protein
-        self.carbs = carbs
+    def __init__(self, food_name, food_fat, food_protein, food_carbs):
+        self.food_name = food_name
+        self.food_fat = food_fat
+        self.food_protein = food_protein
+        self.food_carbs = food_carbs
 
-        def calories(self):
-            calories = self.fat*9+4*(self.protein+self.carbs)
-            return calories
+    def food_calories(self):
+        self.food_calories = 9*self.food_fat + 4*(self.food_protein+self.food_carbs)
+        return self.food_calories
+
+
+food_1 = food("chicken breast", 2, 21, 0)
+print(food_1.food_name, food_1.food_calories() )
